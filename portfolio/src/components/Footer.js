1d80d39782f204405a3750e54ebfa73012952c78
Avdieienko <avdieienko.d@gmail.com>
2023-08-15 15:00:49 +0100

    Update Footer.js

--- portfolio/src/components/Footer.js
+++ portfolio/src/components/Footer.js
@@ -1,9 +1,4 @@
-
-import insta from "../images/insta.webp";
-import twitter from "../images/twitter.webp";
-import facebook from "../images/facebook.webp";
-import github from "../images/github.webp";
-import linkedin from "../images/linkedin.webp";
+import {BsInstagram, BsFacebook, BsTwitter, BsLinkedin, BsGithub} from "react-icons/bs"
 import "../styles/Footer.css"
 
 
@@ -12,13 +7,23 @@ export const Footer = ()=>{
     return(
         <footer>
             <div className="socials">
-                <a rel="noopener noreferrer" target="_blank" href="https://www.instagram.com/avd.dima03/"><img alt="Instagram" src={insta}/></a>
-                <a rel="noopener noreferrer" target="_blank" href="https://www.facebook.com/people/Dmytro-Avdieienko/100009028267852/"><img alt="Facebook"src={facebook}/></a>
-                <a rel="noopener noreferrer" target="_blank" href="https://twitter.com/avdima03"><img alt="Twitter" src={twitter}/></a>
-                <a rel="noopener noreferrer" target="_blank" href="https://www.linkedin.com/in/dmytro-avdieienko-843324217/"><img alt="Linkedin" src={linkedin}/></a>
-                <a rel="noopener noreferrer" target="_blank" href="https://github.com/Avdieienko"><img alt="Linkedin" src={github}/></a>
+                <a rel="noopener noreferrer" target="_blank" href="https://www.instagram.com/avd.dima03/">
+                    <BsInstagram color="#BAFF39" size={25}/>
+                </a>
+                <a rel="noopener noreferrer" target="_blank" href="https://www.facebook.com/people/Dmytro-Avdieienko/100009028267852/">
+                    <BsFacebook color="#BAFF39" size={25}/>
+                </a>
+                <a rel="noopener noreferrer" target="_blank" href="https://twitter.com/avdima03">
+                    <BsTwitter color="#BAFF39" size={25}/>
+                </a>
+                <a rel="noopener noreferrer" target="_blank" href="https://www.linkedin.com/in/dmytro-avdieienko-843324217/">
+                    <BsLinkedin color="#BAFF39" size={25}/>
+                </a>
+                <a rel="noopener noreferrer" target="_blank" href="https://github.com/Avdieienko">
+                    <BsGithub color="#BAFF39" size={25}/>
+                </a>
             </div>
-            <h4 >Designed & Built by <a className="signature" href="/">Dmytro Avdieienko</a></h4>
+            <h4 >Designed & Built by <a className="signature" href="/">VDNK</a></h4>
         </footer>
     )
 
@@ -27,11 +32,21 @@ export const Footer = ()=>{
 export const DesktopFooter = ()=>{
     return(
         <div className="desktop_footer">
-            <a className="top_animation delay-4" rel="noopener noreferrer" target="_blank" href="https://github.com/Avdieienko"><img alt="Github" src={github}/></a>
-            <a className="top_animation delay-3" rel="noopener noreferrer" target="_blank" href="https://www.linkedin.com/in/dmytro-avdieienko-843324217/"><img alt="Linkedin" src={linkedin}/></a>
-            <a className="top_animation delay-2" rel="noopener noreferrer" target="_blank" href="https://twitter.com/avdima03"><img alt="Twitter" src={twitter}/></a>
-            <a className="top_animation delay-1" rel="noopener noreferrer" target="_blank" href="https://www.facebook.com/people/Dmytro-Avdieienko/100009028267852/"><img alt="Facebook" src={facebook}/></a>
-            <a className="top_animation" rel="noopener noreferrer" target="_blank" href="https://www.instagram.com/avd.dima03/"><img alt="Instagram" src={insta}/></a>
+            <a className="top_animation delay-4" rel="noopener noreferrer" target="_blank" href="https://github.com/Avdieienko">
+                <BsGithub color="#BAFF39" size={30}/>
+            </a>
+            <a className="top_animation delay-3" rel="noopener noreferrer" target="_blank" href="https://www.linkedin.com/in/dmytro-avdieienko-843324217/">
+                <BsLinkedin color="#BAFF39" size={30}/>
+            </a>
+            <a className="top_animation delay-2" rel="noopener noreferrer" target="_blank" href="https://twitter.com/avdima03">
+                <BsTwitter color="#BAFF39" size={30}/>
+            </a>
+            <a className="top_animation delay-1" rel="noopener noreferrer" target="_blank" href="https://www.facebook.com/people/Dmytro-Avdieienko/100009028267852/">
+                <BsFacebook color="#BAFF39" size={30}/>
+            </a>
+            <a className="top_animation" rel="noopener noreferrer" target="_blank" href="https://www.instagram.com/avd.dima03/">
+                <BsInstagram color="#BAFF39" size={30}/>
+            </a>
             <hr/>
         </div>
     )
