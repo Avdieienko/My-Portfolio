34ba3c95b848f5d9b3d9fcb697e8c355f4e7ddde
Avdieienko <avdieienko.d@gmail.com>
2023-08-16 18:22:25 +0100

    Include different mobile visual

--- portfolio/src/components/Projects.js
+++ portfolio/src/components/Projects.js
@@ -1,60 +1,58 @@
 import { VisibilityDiv } from "./OnScreen"
-import { useState } from "react"
+import {useState } from "react"
 import "../styles/Projects.css"
 import restaurant from "../images/restaurant.png"
 import flight from "../images/flight.png"
 import statistic from "../images/statistic.png"
 import security from "../images/security.png"
 import sort from "../images/sort.png"
-import python from "../images/python.webp";
-import react_png from "../images/react.webp";
-import html from "../images/html.webp";
-import css from "../images/css.webp";
-import javascript from "../images/javascript.webp";
 import {MdArrowBackIos, MdArrowForwardIos} from "react-icons/md"
-
+import {BiLogoReact, BiLogoHtml5,BiLogoCss3, BiLogoPython,BiLogoJavascript} from "react-icons/bi"
+import {BsGithub} from "react-icons/bs"
+import {useOrientation} from "react-use"
+import "../styles/ProjectsMob.css"
 
 
 
 export const Projects = ()=>{
     const [primary, setPrimary] = useState(2)
 
-
+    const {type} = useOrientation()
 
     const projectsList = [{
         "title":"Security camera",
         "image":security,
-        "skills":[python],
+        "skills":[BiLogoPython],
         "link":"https://github.com/Avdieienko/Security-camera",
         "description":"Detecting the movement and face on the camera, highlighting it, and capturing"
     },
     {
         "title":"Flight Route Analysis",
         "image":flight,
-        "skills":[python],
+        "skills":[BiLogoPython],
         "link":"https://github.com/Avdieienko/FlightAnalysis",
         "description":"Dataframe for each flight, link for scraped website, and a graph with cheapest, average, and maximum ticket price for each day in the given date range."
     },
     {
         "title":"Japanese restaurant",
         "image":restaurant,
-        "skills":[react_png,javascript, html,css],
+        "skills":[BiLogoReact, BiLogoJavascript, BiLogoHtml5, BiLogoCss3],
         "link":"https://avdieienko.com/restaurant/",
         "description":"Website for an imaginary japanese restaurant, with all functionality (except back-end)"
     },
     {
         "title":"Sorting Visualiser",
         "image":sort,
-        "skills":[react_png,javascript, html,css],
+        "skills":[BiLogoReact, BiLogoJavascript, BiLogoHtml5, BiLogoCss3],
         "link":"https://avdieienko.com/sorter",
         "description":"React web app with visualisation of array sorting using different algorithms"
     },
     {
-        "title":"Statistical learning model comparison",
+        "title":"Statistical learning model",
         "image":statistic,
-        "skills":[python],
+        "skills":[BiLogoPython],
         "link":"https://github.com/Avdieienko/statistical_learning_models_comparison",
-        "description":"The comparison graph of two regression and classification models accuracy score for revenue prediction and profitability classification based on a database of 5000 movies catalogued by The Movie Database (TMDb)"
+        "description":"The comparison graph of two regression and classification models accuracy score for revenue prediction and profitability classification of films"
     }]
 
 
@@ -145,61 +143,78 @@ export const Projects = ()=>{
             <VisibilityDiv classname="bottom animation">
                 <h1 className="projects_section_title">Projects</h1>
             </VisibilityDiv>
-            <div className="projects_wrapper">
-                <div className="clicker_left" onClick={()=>{handleClick(-1)}}>
-                    <MdArrowBackIos className="clicker" color="#BAFF39"/>
-                </div>
-                {projectsList.map((project,i)=>{
-                    if(i === primary-1 || i ===primary+1){
-                        return(
-                            <>
-                                <div key={project["title"]} className={`project helper ${i === primary+1 ? "right":"left"}`}>
-                                    <img alt="project" className="project_image helper" src={project["image"]}/>
-                                </div>
-                            </>
-                        )
-                    }
-                    else if(i===primary){
-                        return(
-                            <>
-                                <div key={project["title"]} id={i} className="project">
-                                    <img alt="project" className="project_image" src={project["image"]}/>
-                                </div>
-                            </>
-                        )
-                    }
-                    else{
-                        return(
-                            <>
-                                <div id={i} key={project["title"]} className={`project  invisible ${i>primary ? "right":"left"}`}>
-                                    <img alt="project" className="project_image invisible" src={project["image"]}/>
-                                </div>
-                            </>
-                        )
-                    }
+            {type !== "landscape-primary" ?
+            <div className="projects_wrapper_mobile">
+                {projectsList.map((project, i)=>{
+                    return(
+                        <a href={project.link} className="project_container_mobile">
+                            <div key={i}  className="project_mobile">
+                                    <img alt={project.title} className="project_image_mobile" src={project.image}/>
+                                    <h1 className="project_title_mobile">{project.title}</h1>
+                                    <p className="project_description_mobile">{project.description}</p>
+                            </div>
+                        </a>
+                    )
                 })}
-                <div className="clicker_right" onClick={()=>{handleClick(1)}}>
-                    <MdArrowForwardIos className="clicker" color="#BAFF39"/>
-                </div>
             </div>
-            <div className="project_info">
-                <div>
-                    <h1 className="project_title">{projectsList[primary]["title"]}</h1>
-                </div>
-                <div className="project_skills">
-                    {projectsList[primary]["skills"].map((skill,i)=>{
-                        return(
-                            <img alt={projectsList[primary]["title"]} className="project_skill" src={skill} key={i}></img>
-                        )
+            :<>
+                <div className="projects_wrapper">
+                    <div className="clicker_left" onClick={()=>{handleClick(-1)}}>
+                        <MdArrowBackIos className="clicker" color="#BAFF39"/>
+                    </div>
+                    {projectsList.map((project,i)=>{
+                        if(i === primary-1 || i ===primary+1){
+                            return(
+                                <>
+                                    <div key={project["title"]} className={`project helper ${i === primary+1 ? "right":"left"}`}>
+                                        <img alt="project" className="project_image helper" src={project["image"]}/>
+                                    </div>
+                                </>
+                            )
+                        }
+                        else if(i===primary){
+                            return(
+                                <>
+                                    <div key={project["title"]} id={i} className="project">
+                                        <img alt="project" className="project_image" src={project["image"]}/>
+                                    </div>
+                                </>
+                            )
+                        }
+                        else{
+                            return(
+                                <>
+                                    <div id={i} key={project["title"]} className={`project  invisible ${i>primary ? "right":"left"}`}>
+                                        <img alt="project" className="project_image invisible" src={project["image"]}/>
+                                    </div>
+                                </>
+                            )
+                        }
                     })}
+                    <div className="clicker_right" onClick={()=>{handleClick(1)}}>
+                        <MdArrowForwardIos className="clicker" color="#BAFF39"/>
+                    </div>
                 </div>
-            </div>
-            <div className="project_description_wrapper">
-                <p className="project_description">{projectsList[primary]["description"]}</p>
-            </div>
-            <VisibilityDiv classname="bottom_animation explore">
-                    <a rel="noopener noreferrer" target="_blank" href={projectsList[primary]["link"]} className="certificates_button">Explore</a>
-            </VisibilityDiv>
+                <div className="project_info">
+                    <div>
+                        <h1 className="project_title">{projectsList[primary]["title"]}</h1>
+                    </div>
+                    <div className="project_skills">
+                        {projectsList[primary]["skills"].map((Skill,i)=>{
+                            return(
+                                <Skill key={i} className="project_skill" color="#BAFF39"/>
+                            )
+                        })}
+                    </div>
+                </div>
+                <div className="project_description_wrapper">
+                    <p className="project_description">{projectsList[primary]["description"]}</p>
+                </div>
+                <a rel="noopener noreferrer" target="_blank" href={projectsList[primary]["link"]} className="link_button">Explore</a>
+            </>}
+            <h1 className="projects_git">See more on my Github</h1>
+            <a rel="noopener noreferrer" target="_blank" href="https://github.com/Avdieienko" className="link_button">My Github <BsGithub/></a>
+
         </section>
     )
 }
